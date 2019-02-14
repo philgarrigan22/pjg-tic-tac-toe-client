61ebbbea3e3337bff844094f4758101dbcbb5d65
philgarrigan22 <philgarrigan@comcast.net>
2019-02-14 16:01:23 -0500

    Add user sign-up event listener
    - Add event listener for user sign-up

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,9 +3,11 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const gameEvents = require('./gameplay/events.js')
+const authEvents = require('./auth/events.js')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
   $('.box').on('click', gameEvents.onNewMove)
 })
