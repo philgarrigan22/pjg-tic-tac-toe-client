cd07be0a77715d37b20d846334b2114f2b941c0a
philgarrigan22 <philgarrigan@comcast.net>
2019-02-14 23:37:50 -0500

    Add user sign-out event listener
    - Add event listener for user sign-out option.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,5 +11,6 @@ $(() => {
   $('#sign-up-form').on('submit', authEvents.onSignUp)
   $('#sign-in-form').on('submit', authEvents.onSignIn)
   $('#change-password-form').on('submit', authEvents.onChangePassword)
+  $('#sign-out-button').on('click', authEvents.onSignOut)
   $('.box').on('click', gameEvents.onNewMove)
 })
