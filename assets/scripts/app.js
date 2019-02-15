33c19420f2a669a1f99f82c75d838058280b1339
philgarrigan22 <philgarrigan@comcast.net>
2019-02-14 23:12:30 -0500

    Add user change-password event listener
    - Add event listener for user change-password option.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,5 +10,6 @@ const authEvents = require('./auth/events.js')
 $(() => {
   $('#sign-up-form').on('submit', authEvents.onSignUp)
   $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#change-password-form').on('submit', authEvents.onChangePassword)
   $('.box').on('click', gameEvents.onNewMove)
 })
