cec43b92473023b574eac7e294f5cfa8ebc3c1ce
philgarrigan22 <philgarrigan@comcast.net>
2019-02-17 20:10:53 -0500

    Add new move function
    - Add new move function to initiate PATCH request and update game.

--- assets/scripts/gameplay/api.js
+++ assets/scripts/gameplay/api.js
@@ -6,6 +6,7 @@ const createGame = () => {
   return $.ajax({
     url: config.apiUrl + '/games',
     method: 'POST',
+    contentType: 'application/json',
     headers: {
       Authorization: 'Token token=' + store.user.token
     },
@@ -17,15 +18,37 @@ const showGames = () => {
   return $.ajax({
     url: config.apiUrl + '/games',
     method: 'GET',
+    contentType: 'application/json',
     headers: {
       Authorization: 'Token token=' + store.user.token
     }
-    // contentType: 'application/json',
     // data: JSON.stringify(gameData)
+  })
+}
+
+const newMove = (index, value, over) => {
+  console.log('API newMove is invoked')
+  return $.ajax({
+    url: config.apiUrl + '/games/' + store.gameID,
+    method: 'PATCH',
+    contentType: 'application/json',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: JSON.stringify({
+      'game': {
+        'cells': {
+          'index': index,
+          'value': value
+        },
+        'over': over
+      }
+    })
   })
 }
 
 module.exports = {
   createGame,
-  showGames
+  showGames,
+  newMove
 }
