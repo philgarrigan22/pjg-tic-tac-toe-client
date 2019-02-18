b8760dacb73b3722ed621c87311b942b440c7b12
philgarrigan22 <philgarrigan@comcast.net>
2019-02-17 20:03:16 -0500

    Add update game curl script
    - Add update game PATCH curl script .

---
+++ curl-scripts/gameplay/update-game.sh
@@ -0,0 +1,17 @@
+#!/bin/bash "over":"'"${OVER}"'"
+
+curl "https://tic-tac-toe-wdi.herokuapp.com/games/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Authorization: Token token=${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "game": {
+      "cell": {
+        "index": "'"${INDEX}"'",
+        "value": "'"${VALUE}"'"
+      },
+      "over": "'"${OVER}"'"
+  }'
+
+echo
