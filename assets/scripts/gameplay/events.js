e6a0db4d71ae2aa431fd03db36b9d7ea728804aa
philgarrigan22 <philgarrigan@comcast.net>
2019-02-17 20:12:45 -0500

    Add onNewMove function
    - Add onNewMove function to facilitate game moves.

--- assets/scripts/gameplay/events.js
+++ assets/scripts/gameplay/events.js
@@ -1,6 +1,8 @@
 'use strict'
 const api = require('./api.js')
 const ui = require('./ui.js')
+const store = require('../store.js')
+const gameLogic = require('./gamelogic.js')
 
 const onCreateGame = event => {
   event.preventDefault()
@@ -8,6 +10,8 @@ const onCreateGame = event => {
   api.createGame()
     .then(ui.createGameSuccess)
     .catch(ui.createGameFailure)
+
+  console.log(store.game)
 }
 
 const onShowGames = event => {
@@ -18,8 +22,22 @@ const onShowGames = event => {
     .catch(ui.showGamesFailure)
 }
 
-const onNewMove = () => {
-  console.log('Test Click')
+const onNewMove = (event) => {
+  console.log('events.js onNewMove is invoked')
+  console.log('this is the event target ' + event.target)
+  event.preventDefault()
+
+  const id = $(event.target).data().cellIndex
+  store.id = id
+  // we want to run the api function newMove, and give it
+  api.newMove(store.id, store.player, store.over)
+    .then(ui.newMoveSucess(id))
+  // .catch(ui.newMoveFailure)
+  // then we want to run the ui function newMoveSuccess or newMoveFailure
+  // We also want to run our game logic functions to determine if game is won, switch players, and update the board
+  console.log(store)
+
+  console.log('this is the event target ' + event.target)
 }
 
 module.exports = {
